2c816573e1a0e44a22600f2aef265f82144a5964
Ynna <ynnagumban@gmail.com>
2019-02-08 14:40:55 +0800

    Added home form

---
+++ PYLsystems/Home.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace PYLsystems
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.salesOrderBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.jOrderBtn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.inventoryBtn = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.empManBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 979);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBox4, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox3, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox2, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(163, 100);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(474, 777);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // salesOrderBtn
+            // 
+            this.salesOrderBtn.Location = new System.Drawing.Point(73, 44);
+            this.salesOrderBtn.Name = "salesOrderBtn";
+            this.salesOrderBtn.Size = new System.Drawing.Size(210, 70);
+            this.salesOrderBtn.TabIndex = 0;
+            this.salesOrderBtn.Text = "Sales Order";
+            this.salesOrderBtn.UseVisualStyleBackColor = true;
+            this.salesOrderBtn.Click += new System.EventHandler(this.salesOrderBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.salesOrderBtn);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(50, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 149);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.jOrderBtn);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(50, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(373, 149);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // jOrderBtn
+            // 
+            this.jOrderBtn.Location = new System.Drawing.Point(73, 42);
+            this.jOrderBtn.Name = "jOrderBtn";
+            this.jOrderBtn.Size = new System.Drawing.Size(210, 70);
+            this.jOrderBtn.TabIndex = 0;
+            this.jOrderBtn.Text = "Job Order";
+            this.jOrderBtn.UseVisualStyleBackColor = true;
+            this.jOrderBtn.Click += new System.EventHandler(this.jOrderBtn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.inventoryBtn);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(50, 390);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(373, 149);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // inventoryBtn
+            // 
+            this.inventoryBtn.Location = new System.Drawing.Point(73, 43);
+            this.inventoryBtn.Name = "inventoryBtn";
+            this.inventoryBtn.Size = new System.Drawing.Size(210, 70);
+            this.inventoryBtn.TabIndex = 0;
+            this.inventoryBtn.Text = "Inventory";
+            this.inventoryBtn.UseVisualStyleBackColor = true;
+            this.inventoryBtn.Click += new System.EventHandler(this.inventoryBtn_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.empManBtn);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(50, 545);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(373, 149);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            // 
+            // empManBtn
+            // 
+            this.empManBtn.Location = new System.Drawing.Point(73, 46);
+            this.empManBtn.Name = "empManBtn";
+            this.empManBtn.Size = new System.Drawing.Size(210, 70);
+            this.empManBtn.TabIndex = 0;
+            this.empManBtn.Text = "Employee Management";
+            this.empManBtn.UseVisualStyleBackColor = true;
+            this.empManBtn.Click += new System.EventHandler(this.empManBtn_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 979);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button empManBtn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button inventoryBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button jOrderBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button salesOrderBtn;
+    }
+}
---
+++ PYLsystems/Home.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PYLsystems
+{
+    public partial class Home : Form
+    {
+        int employeeId;
+        int employeeStatus;
+        public Home()
+        {
+            InitializeComponent();
+            /*------temporary. to be erased when combine employee management------*/
+            this.employeeId = 1;
+            this.employeeStatus = 1;
+        }
+        private void Home_Load(object sender, EventArgs e)
+        {
+           /*For employee Status check from Login. 1-admin programmer, 2-Business Owner, 3-Admin staff, 4-Cashier, 5-Framers.
+            Lock Neccessary buttons based on employee Status
+             */
+        }
+        private void salesOrderBtn_Click(object sender, EventArgs e)
+        {
+            checkSales checkSDlg = new checkSales(this.employeeId,this.employeeStatus);
+            checkSDlg.ShowDialog();
+        }
+
+        private void jOrderBtn_Click(object sender, EventArgs e)
+        {
+            frmJobOrderDetails jOrderDlg = new frmJobOrderDetails();
+            jOrderDlg.ShowDialog();
+        }
+
+        private void inventoryBtn_Click(object sender, EventArgs e)
+        {
+            frmInventoryHomePage inventDlg = new frmInventoryHomePage();
+            inventDlg.ShowDialog();
+        }
+
+        private void empManBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
