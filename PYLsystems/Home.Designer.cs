1b6085a5ecb3afd15df45b13aab175778ba3bdb1
benedict966 <43743901+benedict966@users.noreply.github.com>
2019-02-27 23:43:43 +0800

    Update Home.Designer.cs

--- PYLsystems/Home.Designer.cs
+++ PYLsystems/Home.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.salesOrderBtn = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.jOrderBtn = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.inventoryBtn = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.empManBtn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.inventoryBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.jOrderBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.salesOrderBtn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -55,13 +55,15 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 979);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(533, 487);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // tableLayoutPanel2
             // 
@@ -74,7 +76,8 @@
             this.tableLayoutPanel2.Controls.Add(this.groupBox2, 1, 2);
             this.tableLayoutPanel2.Controls.Add(this.groupBox1, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(163, 100);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(108, 50);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 6;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
@@ -83,104 +86,117 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(474, 777);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(315, 385);
             this.tableLayoutPanel2.TabIndex = 0;
-            // 
-            // salesOrderBtn
-            // 
-            this.salesOrderBtn.Location = new System.Drawing.Point(73, 44);
-            this.salesOrderBtn.Name = "salesOrderBtn";
-            this.salesOrderBtn.Size = new System.Drawing.Size(210, 70);
-            this.salesOrderBtn.TabIndex = 0;
-            this.salesOrderBtn.Text = "Sales Order";
-            this.salesOrderBtn.UseVisualStyleBackColor = true;
-            this.salesOrderBtn.Click += new System.EventHandler(this.salesOrderBtn_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.salesOrderBtn);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(50, 80);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(373, 149);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.jOrderBtn);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(50, 235);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(373, 149);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            // 
-            // jOrderBtn
-            // 
-            this.jOrderBtn.Location = new System.Drawing.Point(73, 42);
-            this.jOrderBtn.Name = "jOrderBtn";
-            this.jOrderBtn.Size = new System.Drawing.Size(210, 70);
-            this.jOrderBtn.TabIndex = 0;
-            this.jOrderBtn.Text = "Job Order";
-            this.jOrderBtn.UseVisualStyleBackColor = true;
-            this.jOrderBtn.Click += new System.EventHandler(this.jOrderBtn_Click);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.inventoryBtn);
-            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(50, 390);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(373, 149);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            // 
-            // inventoryBtn
-            // 
-            this.inventoryBtn.Location = new System.Drawing.Point(73, 43);
-            this.inventoryBtn.Name = "inventoryBtn";
-            this.inventoryBtn.Size = new System.Drawing.Size(210, 70);
-            this.inventoryBtn.TabIndex = 0;
-            this.inventoryBtn.Text = "Inventory";
-            this.inventoryBtn.UseVisualStyleBackColor = true;
-            this.inventoryBtn.Click += new System.EventHandler(this.inventoryBtn_Click);
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.empManBtn);
             this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox4.Location = new System.Drawing.Point(50, 545);
+            this.groupBox4.Location = new System.Drawing.Point(33, 271);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(373, 149);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Size = new System.Drawing.Size(248, 73);
             this.groupBox4.TabIndex = 2;
             this.groupBox4.TabStop = false;
             // 
             // empManBtn
             // 
-            this.empManBtn.Location = new System.Drawing.Point(73, 46);
+            this.empManBtn.Location = new System.Drawing.Point(49, 30);
+            this.empManBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.empManBtn.Name = "empManBtn";
-            this.empManBtn.Size = new System.Drawing.Size(210, 70);
+            this.empManBtn.Size = new System.Drawing.Size(140, 45);
             this.empManBtn.TabIndex = 0;
             this.empManBtn.Text = "Employee Management";
             this.empManBtn.UseVisualStyleBackColor = true;
             this.empManBtn.Click += new System.EventHandler(this.empManBtn_Click);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.inventoryBtn);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(33, 194);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(248, 73);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // inventoryBtn
+            // 
+            this.inventoryBtn.Location = new System.Drawing.Point(49, 28);
+            this.inventoryBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inventoryBtn.Name = "inventoryBtn";
+            this.inventoryBtn.Size = new System.Drawing.Size(140, 45);
+            this.inventoryBtn.TabIndex = 0;
+            this.inventoryBtn.Text = "Inventory";
+            this.inventoryBtn.UseVisualStyleBackColor = true;
+            this.inventoryBtn.Click += new System.EventHandler(this.inventoryBtn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.jOrderBtn);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(33, 117);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(248, 73);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // jOrderBtn
+            // 
+            this.jOrderBtn.Location = new System.Drawing.Point(49, 27);
+            this.jOrderBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.jOrderBtn.Name = "jOrderBtn";
+            this.jOrderBtn.Size = new System.Drawing.Size(140, 45);
+            this.jOrderBtn.TabIndex = 0;
+            this.jOrderBtn.Text = "Job Order";
+            this.jOrderBtn.UseVisualStyleBackColor = true;
+            this.jOrderBtn.Click += new System.EventHandler(this.jOrderBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.salesOrderBtn);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(33, 40);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(248, 73);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // salesOrderBtn
+            // 
+            this.salesOrderBtn.Location = new System.Drawing.Point(49, 29);
+            this.salesOrderBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.salesOrderBtn.Name = "salesOrderBtn";
+            this.salesOrderBtn.Size = new System.Drawing.Size(140, 45);
+            this.salesOrderBtn.TabIndex = 0;
+            this.salesOrderBtn.Text = "Sales Order";
+            this.salesOrderBtn.UseVisualStyleBackColor = true;
+            this.salesOrderBtn.Click += new System.EventHandler(this.salesOrderBtn_Click);
+            // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 979);
+            this.ClientSize = new System.Drawing.Size(533, 487);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -190,12 +206,12 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Button empManBtn;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button inventoryBtn;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button jOrderBtn;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button salesOrderBtn;
+        public System.Windows.Forms.Button empManBtn;
+        public System.Windows.Forms.Button inventoryBtn;
+        public System.Windows.Forms.Button jOrderBtn;
+        public System.Windows.Forms.Button salesOrderBtn;
     }
 }
