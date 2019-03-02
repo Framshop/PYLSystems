32506497ad46a97647b3c14f1fcc903bba4922c7
benedict966 <43743901+benedict966@users.noreply.github.com>
2019-03-02 20:10:50 +0800

    Create addEmpForm.cs

---
+++ PYLsystems/EmployeeManagement/addEmpForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PYLsystems.EmployeeManagement
+{
+    public partial class addEmpForm : Form
+    {
+        public delegate void AddEmployeeInstanceEventHandler(addEmpForm source, EventArgs args);
+        public event AddEmployeeInstanceEventHandler AddEmpInstance;
+
+        addEmployeeInstance parentInstance;
+        public addEmpForm(addEmployeeInstance parent)
+        {           
+            InitializeComponent();
+            this.parentInstance = parent;
+            this.birthDatePicker.Value = DateTime.Now;
+
+        }
+
+        protected virtual void nextButton_Click(object sender, EventArgs e)
+        {
+            if (AddEmpInstance != null)
+                AddEmpInstance(this, EventArgs.Empty);
+        }
+    }
+}
