1b6db40e89e94f579310c6a22bfcd9b8be8612e6
cjtgoc-ong30 <cjtgocong30@gmail.com>
2019-03-05 18:59:01 +0800

    Create frmArchiveList.Designer.cs

---
+++ PYLsystems/frmArchiveList.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PYLsystems
+{
+    partial class frmArchiveList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnViewEmp = new System.Windows.Forms.Button();
+            this.btnEmpList = new System.Windows.Forms.Button();
+            this.btnUpdateEmp = new System.Windows.Forms.Button();
+            this.dgEmpList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(62, 24);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(161, 20);
+            this.txtSearch.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 17;
+            // 
+            // btnViewEmp
+            // 
+            this.btnViewEmp.Location = new System.Drawing.Point(18, 321);
+            this.btnViewEmp.Name = "btnViewEmp";
+            this.btnViewEmp.Size = new System.Drawing.Size(101, 43);
+            this.btnViewEmp.TabIndex = 16;
+            this.btnViewEmp.Text = "View Employee";
+            this.btnViewEmp.UseVisualStyleBackColor = true;
+            // 
+            // btnEmpList
+            // 
+            this.btnEmpList.Location = new System.Drawing.Point(294, 321);
+            this.btnEmpList.Name = "btnEmpList";
+            this.btnEmpList.Size = new System.Drawing.Size(101, 43);
+            this.btnEmpList.TabIndex = 15;
+            this.btnEmpList.Text = "Close";
+            this.btnEmpList.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateEmp
+            // 
+            this.btnUpdateEmp.Location = new System.Drawing.Point(156, 321);
+            this.btnUpdateEmp.Name = "btnUpdateEmp";
+            this.btnUpdateEmp.Size = new System.Drawing.Size(101, 43);
+            this.btnUpdateEmp.TabIndex = 14;
+            this.btnUpdateEmp.Text = "Update Employee";
+            this.btnUpdateEmp.UseVisualStyleBackColor = true;
+            // 
+            // dgEmpList
+            // 
+            this.dgEmpList.AllowUserToAddRows = false;
+            this.dgEmpList.AllowUserToDeleteRows = false;
+            this.dgEmpList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgEmpList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEmpList.Location = new System.Drawing.Point(18, 50);
+            this.dgEmpList.Name = "dgEmpList";
+            this.dgEmpList.ReadOnly = true;
+            this.dgEmpList.RowHeadersVisible = false;
+            this.dgEmpList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgEmpList.Size = new System.Drawing.Size(495, 254);
+            this.dgEmpList.TabIndex = 12;
+            // 
+            // frmArchiveList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 384);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnViewEmp);
+            this.Controls.Add(this.btnEmpList);
+            this.Controls.Add(this.btnUpdateEmp);
+            this.Controls.Add(this.dgEmpList);
+            this.Name = "frmArchiveList";
+            this.Text = "frmArchiveList";
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnViewEmp;
+        private System.Windows.Forms.Button btnEmpList;
+        private System.Windows.Forms.Button btnUpdateEmp;
+        private System.Windows.Forms.DataGridView dgEmpList;
+    }
+}
