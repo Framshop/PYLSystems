700eb2e30b50246630df95840c6ef7a265ffc68b
cjtgoc-ong30 <cjtgocong30@gmail.com>
2019-03-05 18:59:35 +0800

    Create frmWorkDesc.cs

---
+++ PYLsystems/frmWorkDesc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PYLsystems
+{
+    public partial class frmWorkDesc : Form
+    {
+        MySqlConnection conn = new MySqlConnection("Server=localhost;Database=frameshopdb;Uid=root;Pwd=root");
+        public frmWorkDesc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string myQuery = "Insert INTO accessworkdesc(employeePosition, workDetailsDesc) values('" + txtWorkPos.Text + "','" + txtWorkDesc.Text + "')";
+            MySqlCommand myComm = new MySqlCommand(myQuery, conn);
+            MySqlDataAdapter myAdp = new MySqlDataAdapter(myComm);
+            DataTable myDt = new DataTable();
+            myAdp.Fill(myDt);
+            conn.Close();
+            MessageBox.Show("Work Description Created");
+
+
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
