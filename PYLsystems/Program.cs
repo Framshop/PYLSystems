f01a5b74cbb56ee7d29c80b48574be28a87c5513
benedict966 <43743901+benedict966@users.noreply.github.com>
2019-03-17 10:57:50 +0800

    Update Program.cs

--- PYLsystems/Program.cs
+++ PYLsystems/Program.cs
@@ -16,7 +16,7 @@ namespace PYLsystems
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new frmSupplyItems());
         }
     }
 }
